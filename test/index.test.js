4d5051ec868721631ecd56585c7189b94e0e19e6
MkDierz <msyibbran24@gmail.com>
2023-07-26 00:43:47 +0700

    feat: :sparkles: enable compressing nested json object

--- test/index.test.js
+++ test/index.test.js
@@ -53,3 +53,68 @@ test('gzip and gunzip work correctly', () => {
 
     expect(gunzipped.toString()).toEqual(bufferOriginal.toString());
 });
+
+test('complex json compress', () => {
+    const data = {
+        products: [
+            {
+                id: 11,
+                title: 'perfume Oil',
+                price: 13,
+            },
+            {
+                id: 12,
+                title: 'Brown Perfume',
+                price: 40,
+            },
+            {
+                id: 13,
+                title: 'Fog Scent Xpressio Perfume',
+                price: 13,
+            },
+            {
+                id: 14,
+                title: 'Non-Alcoholic Concentrated Perfume Oil',
+                price: 120,
+            },
+            {
+                id: 15,
+                title: 'Eau De Perfume Spray',
+                price: 30,
+            },
+            {
+                id: 16,
+                title: 'Hyaluronic Acid Serum',
+                price: 19,
+            },
+            {
+                id: 17,
+                title: 'Tree Oil 30ml',
+                price: 12,
+            },
+            {
+                id: 18,
+                title: 'Oil Free Moisturizer 100ml',
+                price: 40,
+            },
+            {
+                id: 19,
+                title: 'Skin Beauty Serum.',
+                price: 46,
+            },
+            {
+                id: 20,
+                title: 'Freckle Treatment Cream- 15gm',
+                price: 70,
+            },
+        ],
+        total: 100,
+        skip: 10,
+        limit: 10,
+    };
+    expect(() => {
+        const compressed = compress(data);
+        const decompressed = decompress(compressed);
+        return decompressed === data;
+    }).toBeTruthy();
+});
